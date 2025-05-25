7c4deebafe137e75d9bab95ee536ce144c46ab02
rewp <artemblinov480@gmail.com>
2025-05-25 11:12:33 +0300

    Sim without cycle event btw

--- gamesimulator.h
+++ gamesimulator.h
@@ -1,4 +1,22 @@
 #ifndef GAMESIMULATOR_H
 #define GAMESIMULATOR_H
 
+#include <QObject>
+#include "match.h"
+#include <cstdlib>
+#include <ctime>
+
+class GameSimulator : public QObject {
+    Q_OBJECT
+public:
+    explicit GameSimulator(Match* match, QObject* parent = nullptr);
+    void simulateMatchWithLog(MatchLogDialog* logDialog);
+
+signals:
+    void matchFinished(Match* match);
+
+private:
+    Match* m_match;
+};
+
 #endif // GAMESIMULATOR_H
